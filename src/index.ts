18b2a17fb75bdc6a342c9e355bdb074a0be962e9
Daniel Alonge <BrownMarble@outlook.com>
2020-08-17 20:19:59 +0100

    testing objToCss

---
+++ src/index.ts
@@ -0,0 +1,91 @@
+const autoprefixer = require('autoprefixer')
+const postcss = require('postcss')
+const precss = require('precss')
+const fs = require('fs')
+/*
+const processor = postcss( [precss, autoprefixer] )
+processor.process(`
+.btn{
+    color: #fff;
+
+    &:hover{
+        color: blue;
+    }
+}
+`).then((result:any) => console.log(result))
+*/
+const cssObj = {
+  btn: {
+    "@apply": "bg-blue-500 text-white font-bold py-2 px-4 rounded",
+    color: "#fff",
+    "& primary": {
+      color: "blue",
+      "&:hover": {
+        display: "flex"
+      }
+  }
+    
+  }
+}
+
+function isObject(obj:unknown): boolean {
+  return (obj instanceof Object)
+}
+
+function deplicateStr(str:string, times:number){
+  return Array(times).fill(str).join("")
+}
+
+function objToCss(obj: Record<string, any>, level:number = -1){
+  level += 1;
+  let result = "";
+  const tab = deplicateStr("  ", level);
+  
+  console.log({level})
+
+  for(let key in obj){
+    result += tab + key;
+
+    if(obj.hasOwnProperty(key)){
+      
+      if(key.includes("@")){
+        result += " ";
+        result += obj[key];
+        result += ";\n";
+      }
+      else if( isObject(obj[key]) ){
+        result += " {\n";
+        result += objToCss(obj[key], level)
+        result += tab + "}\n";
+      }
+      else{
+        result += ": ";
+        result += obj[key];
+        result += ";\n";
+      }
+       
+      
+    }
+    
+  }
+
+  return result;
+
+}
+
+
+  console.log(objToCss(cssObj))
+
+
+/*
+fs.readFile('src/app.css', (err, css) => {
+  postcss([precss, autoprefixer])
+    .process(css, { from: 'src/app.css', to: 'dest/app.css' })
+    .then(result => {
+      fs.writeFile('dest/app.css', result.css, () => true)
+      if ( result.map ) {
+        fs.writeFile('dest/app.css.map', result.map, () => true)
+      }
+    })
+})
+*/
