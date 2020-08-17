a35ef4374ee462c4c2d4752d14c66736887385f3
Daniel Alonge <BrownMarble@outlook.com>
2020-08-18 00:58:43 +0100

    add plugin/tailwind

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,18 +1,3 @@
-import Tailwind from "../types/tailwind";
-import register from "./register";
-import { isString } from "../lib/type-check";
+import tailwindParser from "./plugin/tailwind";
 
-export default function(tailwindObj: Tailwind): string {
-    
-    return Object.entries(tailwindObj).map( ([k, value]) => {
-        const key = (isString(k) ? k : "") as string;
-        
-        if(key in register){
-            const fn = register[key];
-            return fn(value);
-        }
-
-        return " blank ";
-        
-    }).join("");
-}
+export default tailwindParser;
---
+++ src/parser/plugin/tailwind.ts
@@ -0,0 +1,18 @@
+import Tailwind from "../../types/tailwind";
+import register from "../register";
+import { isString } from "../../lib/type-check";
+
+export default function(tailwindObj: Tailwind): string {
+    
+    return Object.entries(tailwindObj).map( ([k, value]) => {
+        const key = (isString(k) ? k : "") as string;
+        
+        if(key in register){
+            const fn = register[key];
+            return fn(value);
+        }
+
+        return " blank ";
+        
+    }).join("");
+}
