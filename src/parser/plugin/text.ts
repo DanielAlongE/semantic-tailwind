d8695ac3b48a3e268124535244c19bf6f50d8be8
Daniel Alonge <BrownMarble@outlook.com>
2020-08-18 00:54:21 +0100

    add plugin/text

---
+++ src/parser/plugin/text.ts
@@ -0,0 +1,29 @@
+import Text from "../../types/text"
+
+export default function(obj: Text): string {
+        console.log("pasering text")
+    return Object.entries(obj).map( ([key, value]) => {
+
+        if(['opacity'].includes(key)){
+            return ` text-${key}-${value}`
+        }
+        else if (['decoration', 'transform', 'wordBreak', 'fontStyle'].includes(key)){
+            return ` ${value}`
+        }
+        else if(['whitespace'].includes(key)){
+            return ` ${key}-${value}`
+        }
+        else if(['fontWeight', 'fontFamily'].includes(key)){
+            return ` font-${value}`
+        }
+        else if(key == "LetterSpacing"){
+            return ` tracking-${value}`
+        }
+        else if(key == "lineHeight"){
+            return ` leading-${value}`
+        }
+
+        return ` text-${value}`
+    }).join("")
+
+}
--- src/parser/register.ts
+++ src/parser/register.ts
@@ -1,32 +1,4 @@
-import Text from "../types/text"
-
-const text = function(obj: Text): string {
-        console.log("pasering text")
-    return Object.entries(obj).map( ([key, value]) => {
-
-        if(['opacity'].includes(key)){
-            return ` text-${key}-${value}`
-        }
-        else if (['decoration', 'transform', 'wordBreak', 'fontStyle'].includes(key)){
-            return ` ${value}`
-        }
-        else if(['whitespace'].includes(key)){
-            return ` ${key}-${value}`
-        }
-        else if(['fontWeight', 'fontFamily'].includes(key)){
-            return ` font-${value}`
-        }
-        else if(key == "LetterSpacing"){
-            return ` tracking-${value}`
-        }
-        else if(key == "lineHeight"){
-            return ` leading-${value}`
-        }
-
-        return ` text-${value}`
-    }).join("")
-
-}
+import text from "./plugin/text";
 
 const register: Record< string, (o: any)=> string > = {
     text
