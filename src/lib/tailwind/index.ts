8ff82ec9d08ab72627b54233179a4522f66a923d
Daniel Alonge <BrownMarble@outlook.com>
2020-10-23 20:58:17 +0100

    update lib/tailwind/index test types

--- src/lib/tailwind/index.ts
+++ src/lib/tailwind/index.ts
@@ -3,7 +3,7 @@ import TailwindConfig, {Theme} from "../../types/tailwindConfig";
 function typography( configObj: TailwindConfig ){
   let result = [];
   const category = "Typography";
-  configObj.theme.colors.mazamaza[400]
+  configObj.theme
 
   
   
