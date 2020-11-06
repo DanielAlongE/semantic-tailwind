d533e58468ecb10f8e4c4fac2a1a2a32bc910f3d
Daniel Alonge <BrownMarble@outlook.com>
2020-11-06 15:14:58 +0100

    update reactComponentFactory fix bug

--- src/types/reactComponentFactory.ts
+++ src/types/reactComponentFactory.ts
@@ -8,7 +8,7 @@ export interface ComponentData {
   name: string
   as?: string
   baseClass: ClassProp
-  directives?: Record<string, Directive | ClassProp>
+  directives?: Directives
   matched?: Record<string, string>
   computed?: Record<string, string>
   filters?: Record<string, Array<[string, string]>>
