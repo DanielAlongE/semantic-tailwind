9ffa2235535636fc4e6c00776d3e7aa1ce1a89a4
Daniel Alonge <BrownMarble@outlook.com>
2020-11-06 14:03:16 +0100

    update types/index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,5 @@
-import Text from "./text";
-
-export type Screen = 'sm' | 'md' | 'lg' | 'xl'
-
-export interface ScreenTailWind {
-    sm?: Omit<Tailwind, "screen">
-    md?: Omit<Tailwind, "screen">
-    lg?: Omit<Tailwind, "screen">
-    xl?: Omit<Tailwind, "screen">
-}
-
-export default interface Tailwind {
-    text?: Text
-    screen?: ScreenTailWind
-}
+export * from "./color"
+export * from "./reactComponentFactory"
+export * from "./styleConfig"
+export * from "./tailwindConfig"
+export * from "./text"
