268b603962e1cd55ca75e68478f9883e715f0bca
Daniel Alonge <BrownMarble@outlook.com>
2020-11-18 22:42:40 +0100

    update style.config.json

--- src/react/generate.ts
+++ src/react/generate.ts
@@ -1,5 +1,29 @@
+import { ComponentData } from "../types/reactComponentFactory"
+
+export function getComponentPages(comps: ComponentData[]){
+  const groups: Record<string, ComponentData[]> = {}
+  comps.forEach((c) => {
+    const [groupName, childName] = c.name.split(".")
+    if(groupName){
+      if(groupName in groups){
+
+        childName 
+          ? groups[groupName].push(c) 
+          : groups[groupName].unshift(c)
+
+      }else{
+        groups[groupName] = [c]
+      }
+    }
+  })
+
+  return groups
+}
+
+export default function (fileName: string) {
+  const _imports = {}
+  const _lines = []
 
 
-export default function() {
 
 }
