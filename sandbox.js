caf18f1c04c789831a26c92f847a73d7ec49159e
Daniel Alonge <BrownMarble@outlook.com>
2020-11-25 00:30:35 +0100

    update sandbox

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-// const xyz = require("./dist/lib/templateMaker")
-// const tm = xyz.default()
-// const result = tm.addImport("react")
-//   .default("React")
-//     .named(["useEffect", "useRef", "useState"])
 
-//   .addImport("react-subscribed-state")
-//     .named(["useProvider", "useSubscribedState", "SubscribedState as S"])
+  const file = require("./dist/lib/file-handler")
+  const cssHander = require("./dist/lib/css-handler")
 
-//   .addImport("firebase")
-//     .default("* as Firebase")
+  const s = file.read('.\\src\\css\\tailwind.min.css')
 
-//   .addLine("")
-//   .addLine("function Styled(){")
-// .addMultiLine(`// comment 1
-// // comment 2
-// // comment 3
-// `, 4)
-//   .addLine("return (", 1)
-//   .addLine("<div>Hello</div>", 2)
-//   .addLine(")", 1)
-//   .addLine("}")
-//   .toString()
+  const cls = cssHander.getClasses(s).join("\n")
 
-  const xyz = require("./dist/react/generate").default
-
-  xyz('.\\style.config.json')
+  file.write(".\\classList.txt", cls)
