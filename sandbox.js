b6c7d68f7381f88a2d3d17e6ff31b986fee42693
Daniel Alonge <BrownMarble@outlook.com>
2020-11-28 16:57:31 +0100

    checking out getConfigObject

--- sandbox.js
+++ sandbox.js
@@ -3,12 +3,7 @@
 /* eslint-disable no-undef */
 
 // const file = require("./dist/lib/file-handler")
-// const cssHander = require("./dist/lib/css-handler")
+const { getConfigObject } = require("./dist/lib/config-handler")
 
-// const s = file.read('.\\src\\css\\style.css')
 
-// const cls = cssHander.getClasses(s)
-
-// const str = Array.from(new Set(cls)).join("\n")
-
-// file.write(".\\classList.txt", str)
+console.log( getConfigObject() )
