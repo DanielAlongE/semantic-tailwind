8d32717fda1af08c69a58709a3da56cf47bc1351
Daniel Alonge <BrownMarble@outlook.com>
2020-12-31 13:40:34 +0100

    add generator

---
+++ backup/src/generator/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import StyleConfig from "../types/styleConfig";
+
+export default function generator(obj: StyleConfig){
+  obj.components
+}
+
---
+++ backup/src/generator/test.d.ts
@@ -0,0 +1,8 @@
+import React, {FC} from 'react'
+interface buttonProps {
+  primary: boolean
+  secondary: boolean
+  children: any
+}
+const Button: FC<buttonProps>
+export default Button
