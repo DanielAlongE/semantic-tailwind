57e3dbe15a66ba04741f3d6fa88aa0aad0e27105
Daniel Alonge <BrownMarble@outlook.com>
2021-01-02 15:16:00 +0100

    cli add index

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+interface CliOptions {
+   build?: (outDir: string) => void
+}
+
+function cli(options: CliOptions){
+  const { Command } = require('commander');
+  const program = new Command();
+
+  const _build = () => console.log("build argument expected")
+  const build = options.build || _build
+
+  program.version('0.0.1');
+
+  program
+    .command('build')
+    .description('build components from source')
+    .option("-o, --output [output]", "output directory")
+    .action(function({output=""}){
+      build(output)
+    });
+
+
+    program.parse(process.argv);  
+}
+
+if(require.main == module){
+  cli({})
+}
+
+export default cli
