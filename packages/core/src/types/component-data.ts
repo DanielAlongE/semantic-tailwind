320f9460e086f9ead3ff491e86d9bf9c9faf34c8
Daniel Alonge <BrownMarble@outlook.com>
2021-01-03 11:17:36 +0100

    core add component-data

---
+++ packages/core/src/types/component-data.ts
@@ -0,0 +1,15 @@
+export type ClassProp = string[] | string
+
+export type Directive = Record<string, ClassProp | string>
+
+export type Directives = Record<string, Directive | ClassProp>
+
+export interface ComponentData {
+  name?: string
+  as?: string
+  baseClass: ClassProp
+  directives?: Directives
+  matched?: Record<string, string>
+  computed?: Record<string, string>
+  filters?: Record<string, Array<[string, string]>>
+}
