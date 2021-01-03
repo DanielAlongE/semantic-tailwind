425105fedc5a250591ba3b1da745bb4be0dafdb8
Daniel Alonge <BrownMarble@outlook.com>
2021-01-03 16:38:52 +0100

    react bin add #node

--- packages/react/src/bin/index.ts
+++ packages/react/src/bin/index.ts
@@ -1,23 +1,6 @@
+#!/usr/bin/env node
+
 import cli from "semantic-tailwind-cli"
-import * as path from "path"
-import { getConfigObject } from "semantic-tailwind-core"
-import generate from "../lib/generate"
+import build from "./build"
 
-cli({build: function(output){
-  
-  const configObj = getConfigObject()
-
-  const _outDir = output || configObj.outDir
-
-  if(!_outDir){
-    throw new Error("No outDir supplied");
-  }
-
-  const resolvedPath = path.resolve(process.cwd(), _outDir)
-
-  generate(configObj, resolvedPath)
-  .then(() => console.log("Done"))
-  .catch(() => console.log("Oops an error occured!"))
-
-  // console.log(module.filename, output, resolvedPath)
-}})
+cli({build})
