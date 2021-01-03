e9b199da6470a14ab0c8aa6c49c217c5c110220b
Daniel Alonge <BrownMarble@outlook.com>
2021-01-03 11:20:15 +0100

    update indexes

--- packages/playground/src/index.ts
+++ packages/playground/src/index.ts
@@ -1,6 +1,6 @@
-import { getConfig } from "semantic-tailwind-core"
+import { getConfigObject } from "semantic-tailwind-core"
 
 export default function(){
   console.log("playground")
-  console.log(getConfig({some:'some', one:'one', can:'can', outDir:'./some/components'}))
+  console.log(getConfigObject({some:'some', one:'one', can:'can', outDir:'./some/components'}))
 }
--- packages/react/src/bin/index.ts
+++ packages/react/src/bin/index.ts
@@ -1,9 +1,23 @@
 import cli from "semantic-tailwind-cli"
 import * as path from "path"
+import { getConfigObject } from "semantic-tailwind-core"
+import generate from "../lib/generate"
 
 cli({build: function(output){
   
-  const resolvedPath = path.resolve(process.cwd(), output)
+  const configObj = getConfigObject()
 
-  console.log(module.filename, output, resolvedPath)
+  const _outDir = output || configObj.outDir
+
+  if(!_outDir){
+    throw new Error("No outDir supplied");
+  }
+
+  const resolvedPath = path.resolve(process.cwd(), _outDir)
+
+  generate(configObj, resolvedPath)
+  .then(() => console.log("Done"))
+  .catch(() => console.log("Oops an error occured!"))
+
+  // console.log(module.filename, output, resolvedPath)
 }})
