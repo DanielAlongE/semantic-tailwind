e4ba5163fff77ea8f93329c7980b8f961477d1a6
Daniel Alonge <BrownMarble@outlook.com>
2021-01-03 11:11:22 +0100

    react componentFactory

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,36 @@
+import React from "react"
+import { componentDirectivesToClassNames, isString } from "semantic-tailwind-core";
+import { ComponentData } from "semantic-tailwind-core/src/types";
+import { getValidHtmlProp } from "./lib/helper";
+
+export function ComponentFactory(data: ComponentData, _comp?: React.FC<any>){
+  return React.forwardRef((props:any = {}, ref:unknown) => {
+    const {className="", as:renderAs, children=null, key, ...rest} = props
+    //:{className?:string, children?:any, key?:string, rest?:any}
+  
+    const p: any = {}
+  
+    // componentType
+    const comp = renderAs ? renderAs : 
+                    _comp ? _comp : (data.as || "div")
+  
+    if(key){
+      p['key'] = key
+    }
+  
+    if(ref){
+      p['ref'] = ref
+    }
+
+    const _props = isString(comp) ? getValidHtmlProp(rest) : rest
+
+    const cls = componentDirectivesToClassNames(data, rest, className)
+
+    return React.createElement(comp, {...p, className: cls, ..._props}, children )
+  });
+}
+
+
 export default function (){
   console.log('Hello')
 }
