42830de00fcd82e9e6113a29873fff7f966df81a
Daniel Alonge <BrownMarble@outlook.com>
2021-01-03 16:39:42 +0100

    react refactor add bin/build

---
+++ packages/react/src/bin/build.ts
@@ -0,0 +1,22 @@
+import * as path from "path"
+import { getConfigObject } from "semantic-tailwind-core"
+import generate from "../lib/generate"
+
+export default function build(output?:string){
+  
+  const configObj = getConfigObject()
+
+  const _outDir = output || configObj.outDir
+
+  if(!_outDir){
+    throw new Error("No outDir supplied");
+  }
+
+  const resolvedPath = path.resolve(process.cwd(), _outDir)
+
+  generate(configObj, resolvedPath)
+  .then(() => console.log("Done"))
+  .catch(() => console.log("Oops an error occured!"))
+
+  // console.log(module.filename, output, resolvedPath)
+}
