e326f7e1630470001a5ea849528b4e1ba7ce2595
Daniel Alonge <BrownMarble@outlook.com>
2021-01-19 14:55:28 +0100

    render index page ui

--- packages/ui/pages/index.tsx
+++ packages/ui/pages/index.tsx
@@ -1,70 +1,28 @@
 /* eslint-disable no-use-before-define */
+import { DocumentContext } from 'next/dist/next-server/lib/utils'
 import React from 'react'
-// import { ComponentFactory } from 'semantic-tailwind-react'
-import Custom from '../components/Custom'
-// import { getValidHtmlProp } from 'semantic-tailwind-react/dist/lib/helper'
+import Forms from '../components/Forms'
+import { getConfig } from 'semantic-tailwind-react'
 
-// <Custom.Two size="large" primary>I am a button</Custom.Two>
-const ColorShowcase = ({ color }: any) => {
-  return <div>{color}</div>
+function Index ({ data }: any) {
+  const configObj = getConfig()
+  console.log(configObj)
+  return (<Forms data={data} />)
 }
 
-function ColorMapper (Comp: React.FC<any>) {
-  return function (props:any) {
-    const color = props.color === 'Blue' ? 'Read' : props.color
-    return <Comp color={color} />
+Index.getInitialProps = async (ctx: DocumentContext) => {
+  let data = {}
+  const host = ctx.req?.headers.host
+  const time = new Date().getMilliseconds()
+  if (host) {
+    const url = `http://${host}/api/setup?t=${time}`
+    console.log(url)
+    const res = await fetch(url)
+    data = await res.json()
   }
+  // Object.keys(ctx.req?.headers || {})
+  // console.log({ data })
+  return { data }
 }
 
-const MappedColorShowcase = ColorMapper(ColorShowcase)
-
-export default function Index () {
-  // const CustomOne = ComponentFactory({
-  //   baseClass: '',
-  //   as: 'button',
-  //   directives: {
-  //     primary: '',
-  //     size: {
-  //       mini: [
-  //         'text-xs'
-  //       ],
-  //       small: [
-  //         'text-sm'
-  //       ],
-  //       large: [
-  //         'text-lg'
-  //       ],
-  //       string: ''
-  //     }
-  //   },
-  //   name: 'Custom.One'
-  // })
-
-  // const SmallRef = React.forwardRef(({ children, ...props }: any, ref:unknown) => {
-  //   const vProps = getValidHtmlProp(props)
-  //   console.log('Small', { props, vProps })
-  //   return React.createElement('div', { ref, ...vProps }, children)
-  // })
-
-  // const Small = ({ children, ...props }: any) => {
-  //   const vProps = getValidHtmlProp(props)
-  //   console.log('Small', { props, vProps })
-  //   return React.createElement('div', vProps, children)
-  // }
-
-  // const Div = ({ children, ref, ...props }: any) => {
-  //   if (ref) {
-  //     console.log(ref)
-  //   }
-  //   return React.createElement(SmallRef, props, children)
-  // }
-  // getElementControl('Button')({ baseClass: ['show'] })
-  // console.log(Div)
-  //    <Custom.Three ref={console.warn} size="mini" primary>Some one is here</Custom.Three>
-
-  return (<div>
-    <MappedColorShowcase color="Blue" />
-    <h1>Hello</h1>
-    <Custom.Three className="bg-blue-500" as="a" href="wow" size="small" primary={false} ref={console.log}>Give me all</Custom.Three>
-  </div>)
-}
+export default Index
