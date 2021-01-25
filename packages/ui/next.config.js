b4d83907b394c152aed4c46961c82d3432eadaa3
Daniel Alonge <BrownMarble@outlook.com>
2021-01-25 15:07:41 +0100

    webpack test config ui

---
+++ packages/ui/next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.plugins.push(new webpack.IgnorePlugin(/index\.js$/))
+    // Important: return the modified config
+    return config
+  },
+}
