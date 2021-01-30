1ca0f0b07963ee303606b91c4e4edab5b31209b6
Daniel Alonge <BrownMarble@outlook.com>
2021-01-30 15:20:51 +0100

    test functionality in sandbox react

---
+++ packages/react/sandbox.js
@@ -0,0 +1,7 @@
+const { getConfig } = require('./dist')
+
+// console.log(Object.keys(require.cache))
+console.log('-------------------------------------------------------')
+console.log(getConfig())
+
+// console.log(require('C:\\Projects\\semantic-tailwind\\packages\\ui\\semantic-tailwind.config.js'))
