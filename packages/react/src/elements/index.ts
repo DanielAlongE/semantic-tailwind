bc57f1f772ae97acf92f237913d20ca41d842e1c
Daniel Alonge <BrownMarble@outlook.com>
2021-01-31 15:29:48 +0100

    modify getElementControl react

--- packages/react/src/elements/index.ts
+++ packages/react/src/elements/index.ts
@@ -1,21 +1,41 @@
 import React from 'react'
-import { getConfigObject } from 'semantic-tailwind-core'
+import { getConfig } from '../lib/config-handler'
 import DefaultElement from './DefaultElement'
 import Button from './Button'
 
-const elementRegister = {
+const nativeHtmlTags = {
+  div: 'div',
+  button: 'button',
+  a: 'a',
+  details: 'details',
+  dialog: 'dialog',
+  form: 'form',
+  input: 'input',
+  label: 'label',
+  option: 'option',
+  select: 'select',
+  textarea: 'textarea',
+  iframe: 'iframe',
+  img: 'img',
+  table: 'table',
+  td: 'td',
+  th: 'th'
+}
+
+export const defaultElements = {
+  ...nativeHtmlTags,
   Button
 }
 
 type ElementControl = React.FC<any>
 
-export function getElementControl (key: string): ElementControl {
-  const { elements = {} } = getConfigObject()
+export function getElementControl (key: string, configObj: any = null): ElementControl {
+  const { elements = {} } = configObj || getConfig()
 
-  const allElements = { ...elementRegister, ...elements }
+  const _elements = { ...defaultElements, ...elements }
 
-  if (key in allElements) {
-    return allElements[key]
+  if (key in _elements) {
+    return _elements[key]
   }
 
   return DefaultElement
