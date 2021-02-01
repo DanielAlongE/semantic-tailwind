bc772bb21aa778119e41a5a3eb231cece732dcd5
Daniel Alonge <BrownMarble@outlook.com>
2021-02-01 15:41:15 +0100

    remove backup/src/generator

--- backup/src/generator/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import StyleConfig from "../types/styleConfig";
-
-export default function generator(obj: StyleConfig){
-  obj.components
-}
-
