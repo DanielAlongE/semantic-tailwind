d15504e05c90bbb97e4c782acc42e7d1b5375484
Daniel Alonge <BrownMarble@outlook.com>
2021-02-08 15:58:16 +0100

    remove backup

--- backup/sandbox.js
+++
@@ -1,9 +0,0 @@
-
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
-
-// const file = require("./dist/lib/file-handler")
-const { getConfigObject } = require("./dist/lib/config-handler")
-
-
-console.log( getConfigObject() )
--- backup/semantic-tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
-
-const components = require('./style.json')
-
-module.exports = {
-  components,
-  outDir: "./src/components"
-}
